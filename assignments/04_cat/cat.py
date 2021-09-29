1b25cfceb9c7ac4739cc1ee44bd4b29ed4df54a6
root <root@OatbirdSB2.localdomain>
2021-09-29 09:18:55 -0700

    Added cat.py homework, I can't ge tthe test to pass even though it is correct, and the test fails don't tell me what is wrong

---
+++ assignments/04_cat/cat.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+Author : Brian Scott <brianscott@email.arizona.edu>
+Date   : 2021-09-29
+Purpose: Concatenate
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+    parser = argparse.ArgumentParser(
+        description='Rock the Casbah',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('files',
+                        help='A readable file',
+                        metavar='FILE',
+                        type=argparse.FileType('rt'),
+                        nargs='+')
+
+    parser.add_argument('-n',
+                        '--number',
+                        help='Number the lines in the output',
+                        default=False,
+                        action='store_true')
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+    args = get_args()
+    for fh in args.files:
+        counter = 1
+        for line in fh:
+            if args.number == False:
+                print(line.rstrip('\n'))
+            else:
+                print(counter,line.rstrip('\n'))
+                counter += 1
+
+
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
