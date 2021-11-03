d2c784eb01432aeb494e0fb31fd64e4027e37a2c
root <root@OatbirdSB2.localdomain>
2021-11-03 09:14:24 -0700

    can't get past first seq test

--- assignments/09_fasta/au_pair.py
+++ assignments/09_fasta/au_pair.py
@@ -8,6 +8,7 @@ Purpose: Rock the Casbah
 import argparse
 from Bio import SeqIO
 import os
+import sys
 # --------------------------------------------------
 def get_args():
     """Get command-line arguments"""
@@ -36,7 +37,39 @@ def main():
     args = get_args()
     makedir(args)
 
+    outfiles = args.file
+    for files in outfiles:
+        file = files.name
+        basename = os.path.basename(file)
+        root, ext = os.path.splitext(basename)
+        outname1 = os.path.join(args.outdir, root + '_1' + ext)
+        outname2 = os.path.join(args.outdir, root + '_2' + ext)
+
+
+        reader = SeqIO.parse(files, 'fasta')
+
+
+        with open(outname1, 'w') as f:
+            sys.stdout = f
+            for rec in reader:
+                line =+ 1
+                if (line % 2) != 0:
+                    print(rec.description)
+                    print(rec.seq)
+
+
+        with open(outname2, 'w') as f:
+            sys.stdout = f
+            for rec in reader:
+                line =+ 1
+                if (line % 2) == 0:
+                    print(rec.description)
+                    print(rec.seq)
+
     
+
+
+
 def makedir(args):
     directory = args.outdir
     if not os.path.exists(directory):
@@ -48,23 +81,23 @@ def split(args):
 
     
 def out(args):
-    outfiles = args.file
-    outnames = []
-    for files in outfiles:
-        counter = 1
-        namelist = []
-        for char in files.name:
-            namelist.append(char)
-        del namelist[0:namelist.index("/")+1]
-        namelist2 = []
-        for entry in namelist:
-            namelist2.append(entry)
-        namelist.insert(namelist.index("."), "_" + str(counter))
-        outnames.append("".join(namelist))
-        counter += 1
-        namelist2.insert(namelist2.index("."), "_" + str(counter))
-        outnames.append("".join(namelist2))
-    print(outnames)
+    #outfiles = args.file
+    #outnames = []
+    #for files in outfiles:
+    #    counter = 1
+    #    namelist = []
+    #    for char in files.name:
+    #        namelist.append(char)
+    #    del namelist[0:namelist.index("/")+1]
+    #    namelist2 = []
+    #    for entry in namelist:
+    #        namelist2.append(entry)
+    #    namelist.insert(namelist.index("."), "_" + str(counter))
+    #    outnames.append("".join(namelist))
+    #    counter += 1
+    #    namelist2.insert(namelist2.index("."), "_" + str(counter))
+    #    outnames.append("".join(namelist2))
+    #print(outnames)
     teststr = "This is a test"
 
 
