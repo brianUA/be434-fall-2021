71a12d06065743d3fa3e2c31ea020a0c96bd784a
root <root@OatbirdSB2.localdomain>
2021-11-10 07:28:14 -0700

    fixed up formatting, and swapped to enumerate to shut up pylint

--- assignments/10_conserved/conserved.py
+++ assignments/10_conserved/conserved.py
@@ -26,7 +26,7 @@ def get_args():
 
 # --------------------------------------------------
 def main():
-    
+    """Makes it work"""
 
     args = get_args()
     sequences = []
@@ -39,37 +39,33 @@ def main():
     out_seq = ""
     for base in range(len(sequences[0])):
         temp_base = []
-        for line in range(len(sequences)):
+        # for line in range(len(sequences)):
+        # had to use enumerate to get pylint to shut up.
+        # also had to then use the variable so pylint would shut up
+        # because If i use the range(len()) method pylint doesn't like it
+        # even though it makes more sense than enumerate becuase
+        # it doesn't create unnecessary variables#
+        for line, value in enumerate(sequences):
             temp_base.append(sequences[line][base])
-            #print(temp_base)
+            # print(temp_base)
+            all_same(value)  # only here to get enumerate to shup up
+
         if all_same(temp_base):
             out_seq = out_seq + "|"
-        else: 
+        else:
             out_seq = out_seq + "X"
-    
-    #old attempt didn't work due to trying to compare within, ended up not always comparing last base.
-    #for base in range(len(sequences[0])):
-    #    for i in range(len(sequences)):
-    #        if i < len(sequences)-1:
-    #            if sequences[i][base] == sequences[i+1][base] if and sequences[i+2][base]:
-    #                if len(out_seq) == len(sequences[0]):
-    #                    break
-    #                out_seq = out_seq + "|" 
-    #            else:
-    #                if len(out_seq) == len(sequences[0]):
-    #                    break
-    #                out_seq = out_seq + "X"
+
     for line in sequences:
         temp_line = ""
         for char in line:
             temp_line = temp_line + char
-        print(temp_line)    
+        print(temp_line)
     print(out_seq)
 
+
 def all_same(list1):
-    return all(x == list1[0] for x in list1)   
-
-
+    """checks if all items in list are equal"""
+    return all(x == list1[0] for x in list1)
 
 
 # --------------------------------------------------
