a452b73aba658f6ea080a1973d74905934816e61
root <root@OatbirdSB2.localdomain>
2021-11-24 06:36:46 -0700

    Fixed the issue, had the insensitive flag set to default to true

--- assignments/12_grep/grep.py
+++ assignments/12_grep/grep.py
@@ -23,7 +23,7 @@ def get_args():
     parser.add_argument('-i',
                         '--insensitive',
                         help='Case-insensivitive search (default: false)',
-                        action='store_true')
+                        action='store_false')
 
     parser.add_argument('-o',
                         '--outfile',
