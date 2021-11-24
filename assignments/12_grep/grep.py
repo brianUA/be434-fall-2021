51dd88d64a3e7017395c2d3fcde1b7cfdec0ce29
root <root@OatbirdSB2.localdomain>
2021-11-24 06:07:25 -0700

    Added grep.py, got it mostly workoing, but it is printing extra blank lines, and I can't seem to get it to remove them how I want

---
+++ assignments/12_grep/grep.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""
+Author : root <brianscott@email.arizona.edu>
+Date   : 2021-11-22
+Purpose: Search for files
+"""
+
+import argparse
+import sys
+import os
+import re
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Rock the Casbah',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+   
+
+    parser.add_argument('-i',
+                        '--insensitive',
+                        help='Case-insensivitive search (default: false)',
+                        action='store_true')
+
+    parser.add_argument('-o',
+                        '--outfile',
+                        help='specificy output file',
+                        metavar='FILE',
+                        type=argparse.FileType('wt'),
+                        default=sys.stdout)
+
+    parser.add_argument('pattern',
+                        metavar='str',
+                        help='Search pattern')
+
+    parser.add_argument('files',
+                        help='Input file(s)',
+                        metavar='FILE',
+                        nargs='+',
+                        type=argparse.FileType('rt'))
+
+
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    args = get_args()
+    search(args.pattern, args.files, args.insensitive, args.outfile)
+
+def search(pattern, files, sensitive, outfile):
+
+    for fh in files:
+        if len(files) > 1:
+            print(os.path.basename(fh.name), file=outfile)
+        for line in fh:
+            if sensitive:
+                if re.search(pattern, line):
+                    print(line.strip(), file=outfile)
+            else:
+                if re.search(pattern, line, re.I):
+                    print(line.strip(), file=outfile)
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
